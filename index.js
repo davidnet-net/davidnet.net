30b307d8195a190c1282ffcb361ba28b41acf280
Dedestem <david@davidnet.net>
2025-04-05 14:36:29 +0200

    Debug

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { get_session_information } from "./Scripts/Session.js";
+
 window.addEventListener("scroll", function() {
     let scrollMessage = document.getElementById("scrollMessage");
     if (window.scrollY > 100) {
@@ -5,4 +7,9 @@ window.addEventListener("scroll", function() {
     } else {
         scrollMessage.classList.remove("hidden");
     }
+});
+
+document.addEventListener("DOMContentLoaded", async function() {
+    const sessioninfo = await get_session_information();
+    console.log(sessioninfo);
 });
